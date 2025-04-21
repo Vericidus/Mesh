a2233dc768cc0225692c4bfb64a4e1b08d4162b5
Vericidus <vericidus@gmail.com>
2025-04-22 01:33:51 +0530

    Adds baseline performance measurement report with trivial tcp, json operations

--- worker.go
+++ worker.go
@@ -4,12 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"net"
+	"net/http"
+	_ "net/http/pprof"
 	"time"
 )
 
 var ccfg *ComputeConfig
 
 func worker() error {
+	go http.ListenAndServe("localhost:6060", nil)
 	conn, err := net.Dial("tcp", ccfg.ServerAddress)
 	if err != nil {
 		fmt.Printf("[E] Connecting to server at:%s failed.\n", ccfg.ServerAddress)
@@ -17,17 +20,18 @@ func worker() error {
 	}
 
 	enc := json.NewEncoder(conn)
-	dec := json.NewDecoder(conn)
-
-	signal := Signal{
-		"Ping1",
-		ccfg.Id,
-		time.Now(),
-		PING,
-		nil,
-	}
+	dec := json.NewDecoder(conn)	
 
 	for {
+
+		signal := Signal{
+			"Ping1",
+			ccfg.Id,
+			time.Now(),
+			PING,
+			nil,
+		}
+
 		enc.Encode(signal)
 		var response Signal
 		if err := dec.Decode(&response); err != nil {
